c6dbce8204fda7535d82ed95bc0256eb4b0fcd87
Matschieu <mathieudietsch@yahoo.fr>
2024-10-20 01:19:24 +0200

    Add colors on folders in the tree

--- tree.php
+++ tree.php
@@ -12,5 +12,5 @@ $app = FileViewerApplication::build();
 
 <?php foreach($app->getRootFolder()->getFlatTree() as $treeElement) { ?>
 <a href="<?php echo $app->getChangeFolderUrl($treeElement->getFolder()) ?>" class="list-group-item" style="padding-left: <?php echo (20 + $treeElement->getLevel() * 20) ?>px">
-<span class="<?php echo $treeElement->getFolder()->getGlyphicon() ?>"></span> <?php echo $treeElement->getFolder()->getName() ?></a>
+<span class="tree-folder-icon <?php echo $treeElement->getFolder()->getGlyphicon() ?>"></span> <?php echo $treeElement->getFolder()->getName() ?></a>
 <?php } ?>
