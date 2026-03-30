ac4ada4e2948a8e0bde24daee618320357db6681
Matschieu <mathieudietsch@yahoo.fr>
2026-03-30 23:59:35 +0200

    Upgrade Bootstrap from 5.2.3 to 5.3.8

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ $files = $currentFolder->getFileChildren();
 	<meta name="robots" content="noindex" />
 	<link rel="icon" type="image/png" href="<?php echo $app->getFavicon() ?>" />
 	<!--Bootstrap -->
-	<link rel="stylesheet" type="text/css" href="./lib/bootstrap-5.2.3-dist/css/bootstrap.min.css" />
-	<script src="./lib/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="./lib/bootstrap-5.3.8-dist/css/bootstrap.min.css" />
+	<script src="./lib/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
 	<!-- Glyphicons -->
 	<!-- Icons list: https://fontawesome.com -->
 	<link rel="stylesheet" type="text/css" href="./lib/fontawesome-7.2.0/css/fontawesome.min.css" />
